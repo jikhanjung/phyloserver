eeb7104c6ac371c39e7326f6ab060a0c094fb0fc
Jikhan Jung <honestjung@gmail.com>
2022-04-21 23:26:50 +0900

    working on polling/analyzing script

--- phylomanager/management/commands/run_analysis.py
+++ phylomanager/management/commands/run_analysis.py
@@ -1,5 +1,7 @@
 from phylomanager.models import PhyloRun, PhyloPackage, PhyloModel, PhyloLeg
 from django.core.management.base import BaseCommand
+import subprocess
+
 
 class Command(BaseCommand):
     help = "Customized load data for DB migration"
@@ -9,9 +11,14 @@ class Command(BaseCommand):
         for run in runs:
             print( run, run.get_run_status_display() )
             for leg in run.leg_set.all():
+                package = leg.leg_package;
                 print( "  ",leg, leg.get_leg_status_display() )
+                print("gonna execute", package, package.get_package_type_display(), "at", package.run_path)
+                if package.package_name == 'IQTree':
+                    # create run directory, copy file, run IQTree
+                    
+                    subprocess.run([ package.run_path, "-i", run.)
 
     def get_candidate_runs(self):
         runs = PhyloRun.objects.filter(run_status__in=['QD','IP']).order_by('created_datetime')
         return runs
-
